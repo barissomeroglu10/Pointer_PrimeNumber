c5e6be28c3a852d840324996e5b058f5b913bb49
baris <baris@BARIS_SOMEROGLU>
2024-04-28 18:19:09 +0300

    Add project files.

---
+++ PrimeNumber/PrimeNumber.cpp
@@ -0,0 +1,54 @@
+﻿/*
+This code shows prime number from 1 to enter number by user
+
+Developer = Barış Someroğlu
+Date = 28.04.2024 - 06:10 pm
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+void PrimeNumberList(int*);
+bool IsItPrimeNumber(int);
+
+int main()
+{
+	int Number;
+
+	cout << "Please enter a number = ";
+	cin >> Number;
+
+	cout << endl;
+
+	int* ptrNumber = &Number;
+
+	// Call function
+	PrimeNumberList(&Number);
+
+	return 0;
+}
+
+// Function definition
+void PrimeNumberList(int* ptrNumber)
+{
+	for (int i = 1; i <= *ptrNumber; i++)
+	{
+		if (IsItPrimeNumber(i) == true) // Call function in a function
+			cout << i << endl;
+	}
+}
+
+// Function definition
+bool IsItPrimeNumber(int Control)
+{
+	for (int j = 2; j < Control; j++)
+	{
+		if (Control % j == 0)
+			return false;
+	}
+
+	return true;
+}
